83dc28671634fb743cd6bdedda6773a6d5ca4976
guilherme soares <guilherme.dev36@gmail.com>
2022-11-30 14:32:18 -0300

    feat: add styles, @types, screen home and components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,30 @@
+import { View, Image, FlatList } from "react-native";
+
+import LogoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+
+import { styles } from "./styles";
+
+import { GAMES } from "../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={LogoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+}
